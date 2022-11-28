4bea0da3c3d06f6bbf59cf320c84edf49d8510af
jyi2ya <jyi2ya@outlook.com>
2022-11-28 19:06:53 +0800

    more strict test

--- scripts/test-dir.sh
+++ scripts/test-dir.sh
@@ -17,8 +17,8 @@ do_test() {
         ../evenodd write "$filename" "$p"
 
         for _ in 0 1 2; do
-            ../evenodd read "$filename" test.bin.rtv
-            diff "$filename" test.bin.rtv || exit 2
+            ../evenodd read "$filename" "$PWD/test.bin.rtv"
+            diff "$filename" "$PWD/test.bin.rtv" || exit 2
             rm -rf "disk_$((RANDOM % (p + 2)))"
         done
 
@@ -27,13 +27,13 @@ do_test() {
         bad1=$((RANDOM % (p + 2)))
         rm -rf "disk_$bad1"
         ../evenodd repair 1 "$bad1"
-        ../evenodd read "$filename" test.bin.rtv
-        diff "$filename" test.bin.rtv || exit 2
+        ../evenodd read "$filename" "$PWD/test.bin.rtv"
+        diff "$filename" "$PWD/test.bin.rtv" || exit 2
         bad2=$((RANDOM % (p + 2)))
         rm -rf "disk_$bad1" "disk_$bad2"
         ../evenodd repair 2 "$bad1" "$bad2"
-        ../evenodd read "$filename" test.bin.rtv
-        diff "$filename" test.bin.rtv || exit 2
+        ../evenodd read "$filename" "$PWD/test.bin.rtv"
+        diff "$filename" "$PWD/test.bin.rtv" || exit 2
     done
 }
 
